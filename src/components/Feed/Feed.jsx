c0a923ceabca80a45162346dd0edd3f5ecedfd13
mantoshdev <123961664+mantoshdev2@users.noreply.github.com>
2023-07-27 16:27:48 +0530

    getting Posts data from firebase db  on console

--- src/components/Feed/Feed.jsx
+++ src/components/Feed/Feed.jsx
@@ -1,8 +1,24 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import CreatePost from '../CreatePost/CreatePost'
 import Post from '../Post/Post'
+import { collection, onSnapshot } from 'firebase/firestore';
+import { db } from '../../firebase';
 
 const Feed = () => {
+    const [posts, setPosts] = useState([]);
+
+    useEffect(() => {
+      const unSub = onSnapshot(collection(db, "posts"), (snapshot) => {
+        setPosts(snapshot.docs.map((doc) => ({ id: doc.id, data: doc.data() })));
+      });
+      return () => {
+        unSub();
+      };
+    }, []);
+    console.log("posts",posts);
+
+
+    
   return (
     <div>
         <CreatePost />
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,9 @@
 import React from 'react'
 import './Home.css'
  
-import CreatePost from '../../components/CreatePost/CreatePost'
+
 import LeftSidebar from '../../components/LeftSidebar/LeftSidebar'
 import RightSidebar from '../../components/RightSidebar/RightSidebar.jsx'
-import Post from '../../components/Post/Post.jsx'
 import Feed from '../../components/Feed/Feed.jsx'
 
 
