0f622ec9fce5b88db08f899d54e6972899730ef2
mantoshdev <123961664+mantoshdev2@users.noreply.github.com>
2023-07-28 18:55:12 +0530

    Implemented add comment feature to the Post

--- src/components/Post/Post.jsx
+++ src/components/Post/Post.jsx
@@ -8,18 +8,27 @@ import { BsSend } from "react-icons/bs";
 import { AiOutlineLike } from "react-icons/ai";
 import { useContext, useEffect, useState } from "react";
 import { AuthContext } from "../../context/AuthContext";
-import { collection, deleteDoc, doc, onSnapshot, setDoc } from "firebase/firestore";
+import {
+  addDoc,
+  collection,
+  deleteDoc,
+  doc,
+  onSnapshot,
+  serverTimestamp,
+  setDoc,
+} from "firebase/firestore";
 import { db } from "../../firebase";
 import { AiFillLike } from "react-icons/ai";
 
-
-
-const Post = ({post}) => {
+const Post = ({ post }) => {
   const { currentUser } = useContext(AuthContext);
   const [likes, setLikes] = useState([]);
   const [liked, setLiked] = useState(false);
+  const [comments, setComments] = useState([]);
+  const [commentOpen, setCommentOpen] = useState(false);
+  const [commentBoxVisible, setCommentBoxVisible] = useState(false);
+  const [input, setInput] = useState("");
 
-  
   useEffect(() => {
     const unSub = onSnapshot(
       collection(db, "posts", post.id, "likes"),
@@ -43,20 +52,51 @@ const Post = ({post}) => {
       });
     }
   };
-  
+
+  // console.log(comments);
+
+  useEffect(() => {
+    const unSub = onSnapshot(
+      collection(db, "posts", post.id, "comments"),
+      (snapshot) => {
+        setComments(
+          snapshot.docs.map((snapshot) => ({
+            id: snapshot.id,
+            data: snapshot.data(),
+          }))
+        );
+      }
+    );
+    return () => {
+      unSub();
+    };
+  }, [post.id]);
+
+  const handleComment = async (e) => {
+    e.preventDefault();
+
+    await addDoc(collection(db, "posts", post.id, "comments"), {
+      comment: input,
+      displayName: currentUser.displayName,
+      photoURL: currentUser.photoURL,
+      uid: currentUser.uid,
+      timestamp: serverTimestamp(),
+    });
+    setCommentBoxVisible(false);
+    setInput("");
+  };
 
   return (
     <>
       <div className="Post">
         <div className="Post-author">
           <img src={post.data?.photoURL} alt="" />
-          
-          
+
           <div>
             <h1>{post.data.displayName}</h1>
             <label>{}</label>
             <label>
-            {new Date(post.data?.timestamp?.toDate()).toLocaleString()}
+              {new Date(post.data?.timestamp?.toDate()).toLocaleString()}
             </label>
           </div>
         </div>
@@ -74,59 +114,114 @@ const Post = ({post}) => {
               className="poststatsicon"
               style={{ color: "#011631" }}
             />
-            {likes.length > 0 && (
-              <span className="">{likes.length}</span>
-            )}
+            {likes.length > 0 && <span className="">{likes.length}</span>}
 
             <FcIdea className="poststatsicon" />
             <PiHandsClappingFill className="poststatsicon" />
             <span className="liked-users">Rahul and 50 others</span>
           </div>
-          <div>
-            <span>20 comments &middot; 40 shares</span>
+          <div className="postBottomRight">
+            <span
+              className="postCommentText"
+              onClick={() => setCommentOpen(!commentOpen)}
+            >
+              {comments.length} comments &middot; 40 shares
+            </span>
           </div>
         </div>
         <div className="post-activity">
-        <div>
-          <img src={currentUser?.photoURL} alt="" className="post-activity-user-icon" />
-          <AiFillCaretDown className="post-activity-arrow-icon" />
+          <div>
+            <img
+              src={currentUser?.photoURL}
+              alt=""
+              className="post-activity-user-icon"
+            />
+            <AiFillCaretDown className="post-activity-arrow-icon" />
           </div>
 
-        <div className="post-acitivity-link"     onClick={(e) => {
+          <div
+            className="post-acitivity-link"
+            onClick={(e) => {
               likePost();
-            }}>
-          {/* <AiOutlineLike className="post-activity-link-icon" /> */}
+            }}
+          >
+            {/* <AiOutlineLike className="post-activity-link-icon" /> */}
 
-      
-          
             {liked ? (
-              <AiFillLike style={{ color: "#011631" }} className="footerIcon" />
+              <AiFillLike
+                style={{ color: "#011631" }}
+                className="post-activity-link-icon"
+              />
             ) : (
-              <AiOutlineLike className="footerIcon" />
-            )}   
+              <AiOutlineLike className="post-activity-link-icon" />
+            )}
 
-          <span>Like</span>
+            <span>Like</span>
+          </div>
+
+          <div
+            className="post-acitivity-link"
+            onClick={() => setCommentBoxVisible(!commentBoxVisible)}
+          >
+            <PiChatsThin className="post-activity-link-icon" />
+            <span>Comment</span>
+          </div>
+          <div className="post-acitivity-link">
+            <PiShareFat className="post-activity-link-icon" />
+            <span>Share</span>
+          </div>
+
+          <div className="post-acitivity-link">
+            <BsSend className="post-activity-link-icon" />
+            <span>Send</span>
+          </div>
+
+          
         </div>
 
-        <div className="post-acitivity-link">
-          <PiChatsThin className="post-activity-link-icon" />
-          <span>Comment</span>
-        </div>
-        <div className="post-acitivity-link">
-          <PiShareFat className="post-activity-link-icon" />
-          <span>Share</span>
-        </div>
+        {commentBoxVisible && (
+            <form onSubmit={handleComment} className="commentBox">
+              <textarea
+                type="text"
+                placeholder="Write a comment ..."
+                className="commentInput"
+                rows={1}
+                style={{ resize: "none" }}
+                value={input}
+                onChange={(e) => setInput(e.target.value)}
+              />
+              <button type="submit" disabled={!input} className="commentPost">
+                Comment
+              </button>
+            </form>
+          )}
 
-        <div className="post-acitivity-link">
-          <BsSend className="post-activity-link-icon" />
-          <span>Send</span>
-        </div>
-        </div>
+          {commentOpen > 0 && (
+            <div className="comment">
+              {comments
+                .sort((a, b) => b.data.timestamp - a.data.timestamp)
+                .map((c) => (
+                  <div>
+                    <div className="commentWrapper">
+                      <img
+                        className="commentProfileImg"
+                        src={c.data.photoURL}
+                        alt=""
+                      />
+                      <div className="commentInfo">
+                        <span className="commentUsername">
+                          @{c.data.displayName.replace(/\s+/g, "").toLowerCase()}
+                        </span>
+                        <p className="commentText">{c.data.comment}</p>
+                      </div>
+                    </div>
+                  </div>
+                ))}
+            </div>
+          )}
       </div>
     </>
   );
-
 };
-
 
 export default Post;
