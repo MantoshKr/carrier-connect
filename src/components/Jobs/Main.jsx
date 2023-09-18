4a516b581ad831ae41076d642deba10013c90c21
mantoshdev <123961664+mantoshdev2@users.noreply.github.com>
2023-09-18 15:36:50 +0530

    Created Main jobs component added to Jobs page

---
+++ src/components/Jobs/Main.jsx
@@ -0,0 +1,365 @@
+import React from "react";
+import cclogo from "../../assets/images/cclogo.png";
+import { BiBookmark } from "react-icons/bi";
+import { HiSearch } from "react-icons/hi";
+
+const Main = () => {
+  return (
+    
+    <div className="flex flex-col gap-6 w-full">
+    {/* suggested job searches */}
+      <div className="flex flex-col max-h-fit w-full items-center justify-center overflow-hidden bg-white rounded shadow-md text-slate-500 shadow-slate-200 sm:flex-row">
+        <div className="flex-1 p-6 sm:mx-6 sm:px-0">
+          <header className="flex justify-between mb-4">
+            <div>
+              <div className="flex gap-4 items-center justify-center">
+                <div>
+                  <h3 className="text-2xl font-bold text-slate-800">
+                    Suggested job searches
+                  </h3>
+                </div>
+              </div>
+            </div>
+            <p className="text-xl"></p>
+          </header>
+
+          <div className="flex flex-wrap ">
+            <button
+              type="button"
+              className="px-4 py-1 text-lg font-semibold rounded-full border mt-2 border-green-600 hover:bg-green-100 flex gap-2 ml-2"
+            >
+              <span className="text-2xl text-green-600 flex items-center justify-center font-bold">
+                <HiSearch />
+              </span>{" "}
+              <span className="text-green-600">marketing manager</span>
+            </button>
+            <button
+              type="button"
+              className="px-4 py-1 text-lg font-semibold rounded-full border mt-2 border-green-600 hover:bg-green-100 flex gap-2 ml-2"
+            >
+              <span className="text-2xl text-green-600 flex items-center justify-center font-bold">
+                <HiSearch />
+              </span>{" "}
+              <span className="text-green-600">hr</span>
+            </button>
+            <button
+              type="button"
+              className="px-4 py-1 text-lg font-semibold rounded-full border mt-2 border-green-600 hover:bg-green-100 flex gap-2 ml-2"
+            >
+              <span className="text-2xl text-green-600 flex items-center justify-center font-bold">
+                <HiSearch />
+              </span>{" "}
+              <span className="text-green-600">legal</span>
+            </button>
+            <button
+              type="button"
+              className="px-4 py-1 text-lg font-semibold rounded-full border mt-2 border-green-600 hover:bg-green-100 flex gap-2 ml-2"
+            >
+              <span className="text-2xl text-green-600 flex items-center justify-center font-bold">
+                <HiSearch />
+              </span>{" "}
+              <span className="text-green-600">sales</span>
+            </button>
+            <button
+              type="button"
+              className="px-4 py-1 text-lg font-semibold rounded-full border mt-2 border-green-600 hover:bg-green-100 flex gap-2 ml-2"
+            >
+              <span className="text-2xl text-green-600 flex items-center justify-center font-bold">
+                <HiSearch />
+              </span>{" "}
+              <span className="text-green-600">amazon</span>
+            </button>
+            <button
+              type="button"
+              className="px-4 py-1 text-lg font-semibold rounded-full border mt-2 border-green-600 hover:bg-green-100 flex gap-2 ml-2"
+            >
+              <span className="text-2xl text-green-600 flex items-center justify-center font-bold">
+                <HiSearch />
+              </span>{" "}
+              <span className="text-green-600">google</span>
+            </button>
+            <button
+              type="button"
+              className="px-4 py-1 text-lg font-semibold rounded-full border mt-2 border-green-600 hover:bg-green-100 flex gap-2 ml-2"
+            >
+              <span className="text-2xl text-green-600 flex items-center justify-center font-bold">
+                <HiSearch />
+              </span>{" "}
+              <span className="text-green-600">analyst</span>
+            </button>
+          </div>
+        </div>
+      </div>
+      {/* try premium for free */}
+      <div className="flex flex-col max-h-fit w-full items-center justify-center overflow-hidden bg-white rounded-lg shadow-lg text-slate-500 shadow-slate-200 sm:flex-row ">
+        <div className="flex-1 p-6 sm:mx-6 sm:px-0">
+          <header className="flex justify-between mb-4 mt-6">
+            <div>
+              <div className="flex gap-4 items-center justify-center">
+                <p
+                  
+                  className="relative inline-flex items-center justify-center h-12 text-white "
+                >
+                  <img
+                    src="https://images.unsplash.com/photo-1568602471122-7832951cc4c5?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80"
+                    alt="user name"
+                    title="user name"
+                    className="max-w-full rounded-full w-20 "
+                  />
+                </p>
+                <div>
+                  <h3 className="text-xl  font-bold text-slate-700">
+                    See the full list of jobs where you'd be a top applicant
+                  </h3>
+                  <div>
+                    <div className="flex items-center justify-center gap-2 m-3">
+                      <div className="flex -space-x-3">
+                        <img
+                          alt=""
+                          className="w-8 h-8 border rounded-full dark:bg-gray-500 dark:border-gray-700"
+                          src="https://source.unsplash.com/40x40/?portrait?1"
+                        />
+                        <img
+                          alt=""
+                          className="w-8 h-8 border rounded-full dark:bg-gray-500 dark:border-gray-700"
+                          src="https://source.unsplash.com/40x40/?portrait?2"
+                        />
+                        <img
+                          alt=""
+                          className="w-8 h-8 border rounded-full dark:bg-gray-500 dark:border-gray-700"
+                          src="https://source.unsplash.com/40x40/?portrait?3"
+                        />
+                        <img
+                          alt=""
+                          className="w-8 h-8 border rounded-full dark:bg-gray-500 dark:border-gray-700"
+                          src="https://source.unsplash.com/40x40/?portrait?4"
+                        />
+                      </div>
+                      <span className="text-sm ">
+                        Millions of members use Premium
+                      </span>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <p className="text-xl"></p>
+          </header>
+          <a
+            class="inline-block rounded-full bg-orange-300 px-10 py-1.5 text-xl font-bold text-black transition hover:scale-110 hover:shadow-xl focus:outline-none focus:ring active:bg-orange-400  ml-10"
+            href="/trypremium"
+          >
+            Try Premium for free
+          </a>
+        </div>
+      </div>
+      <div className="flex flex-col max-h-fit w-full items-center justify-center overflow-hidden bg-white rounded shadow-md text-slate-500 shadow-slate-200 sm:flex-row">
+        {/* update profile */}
+        <div className="flex-1 p-6 sm:mx-6 sm:px-0">
+          <header className="flex justify-between mb-4">
+            <div>
+              <div className="flex gap-4 items-center justify-center">
+                <p
+                  
+                  className="relative inline-flex items-center justify-center h-12 text-white "
+                >
+                  <img
+                    src="https://images.unsplash.com/photo-1568602471122-7832951cc4c5?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80"
+                    alt="user"
+                    title="user"
+                    className="max-w-full rounded-full w-16 "
+                  />
+                </p>
+                <div>
+                  <h3 className="text-xl font-medium text-slate-700">
+                    Complete profiles receive 40x more opportunities
+                  </h3>
+                </div>
+              </div>
+            </div>
+            <p className="text-xl"></p>
+          </header>
+          <a
+            class="inline-block rounded-full bg-white border border-green-600 px-10 py-1.5 text-xl font-bold text-green-600 hover:bg-green-100  "
+            href="/trypremium"
+          >
+            Update Profile
+          </a>
+        </div>
+      </div>
+      <div>
+        {/* jobs */}
+        <div className="flex flex-col max-h-fit w-full items-center justify-center overflow-hidden bg-white rounded shadow-md text-slate-500 shadow-slate-200 sm:flex-row">
+          <div className="flex-1 p-6 sm:mx-6 sm:px-0">
+            <header className="flex justify-between mb-0">
+              <div>
+                <div className="flex gap-4 items-center justify-center">
+                  <p
+                    
+                    className="relative inline-flex items-center justify-center h-12 text-white "
+                  >
+                    <img
+                      src="https://png.pngtree.com/png-vector/20190612/ourmid/pngtree-hospitalbuildingclinicmedical--flat-color-icon--vector-ico-png-image_1339045.jpg"
+                      alt="user name"
+                      title="user name"
+                      className="max-w-full w-16 "
+                    />
+                  </p>
+                  <div>
+                    <h3 className="text-xl font-bold text-sky-500 ">
+                      Dermatologist
+                    </h3>
+                    <h3 className="text-lg font-medium text-slate-600">
+                      Satya Skin & Hair care pvt ltd
+                    </h3>
+                    <p className="text-lg font-medium text-slate-400">
+                      {" "}
+                      Gurugram, Haryana, India (On-site)
+                    </p>
+                  </div>
+                </div>
+              </div>
+              <p className="text-3xl pt-5">
+                <BiBookmark />
+              </p>
+            </header>
+            <p className="text-md ml-20 flex items-center">
+              <span>Promoted</span>
+              <img src={cclogo} alt="logo" className="w-14" />
+              <span>Easy Apply</span>
+            </p>
+          </div>
+        </div>
+        <hr />
+        <div className="flex flex-col max-h-fit w-full items-center justify-center overflow-hidden bg-white rounded shadow-md text-slate-500 shadow-slate-200 sm:flex-row">
+          <div className="flex-1 p-6 sm:mx-6 sm:px-0">
+            <header className="flex justify-between mb-0">
+              <div>
+                <div className="flex gap-4 items-center justify-center">
+                  <p
+                    href="#"
+                    className="relative inline-flex items-center justify-center h-12 text-white "
+                  >
+                    <img
+                      src="https://png.pngtree.com/png-vector/20190612/ourmid/pngtree-hospitalbuildingclinicmedical--flat-color-icon--vector-ico-png-image_1339045.jpg"
+                      alt="job"
+                      title="user "
+                      className="max-w-full w-16 h-16 "
+                    />
+                  </p>
+                  <div>
+                    <h3 className="text-xl font-bold text-sky-500 ">
+                      Dermatologist
+                    </h3>
+                    <h3 className="text-lg font-medium text-slate-600">
+                      Satya Skin & Hair care pvt ltd
+                    </h3>
+                    <p className="text-lg font-medium text-slate-400">
+                      {" "}
+                      Gurugram, Haryana, India (On-site)
+                    </p>
+                  </div>
+                </div>
+              </div>
+              <p className="text-3xl pt-5">
+                <BiBookmark />
+              </p>
+            </header>
+            <p className="text-md ml-20 flex items-center">
+              <span>Promoted</span>
+              <img src={cclogo} alt="logo" className="w-14" />
+              <span>Easy Apply</span>
+            </p>
+          </div>
+        </div>
+        <hr />
+        <div className="flex flex-col max-h-fit w-full items-center justify-center overflow-hidden bg-white rounded shadow-md text-slate-500 shadow-slate-200 sm:flex-row">
+          <div className="flex-1 p-6 sm:mx-6 sm:px-0">
+            <header className="flex justify-between mb-0">
+              <div>
+                <div className="flex gap-4 items-center justify-center">
+                  <p
+                    href="#"
+                    className="relative inline-flex items-center justify-center h-12 text-white "
+                  >
+                    <img
+                      src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/44/Microsoft_logo.svg/2048px-Microsoft_logo.svg.png"
+                      alt="user name"
+                      title="user name"
+                      className="max-w-full w-16 "
+                    />
+                  </p>
+                  <div>
+                    <h3 className="text-xl font-bold text-sky-500 ">
+                      Dermatologist
+                    </h3>
+                    <h3 className="text-lg font-medium text-slate-600">
+                      Satya Skin & Hair care pvt ltd
+                    </h3>
+                    <p className="text-lg font-medium text-slate-400">
+                      {" "}
+                      Gurugram, Haryana, India (On-site)
+                    </p>
+                  </div>
+                </div>
+              </div>
+              <p className="text-3xl pt-5">
+                <BiBookmark />
+              </p>
+            </header>
+            <p className="text-md ml-20 flex items-center">
+              <span>Promoted</span>
+              <img src={cclogo} alt="user" className="w-14" />
+              <span>Easy Apply</span>
+            </p>
+          </div>
+        </div>
+        <hr />
+        <div className="flex flex-col max-h-fit w-full items-center justify-center overflow-hidden bg-white rounded shadow-md text-slate-500 shadow-slate-200 sm:flex-row">
+          <div className="flex-1 p-6 sm:mx-6 sm:px-0">
+            <header className="flex justify-between mb-0">
+              <div>
+                <div className="flex gap-4 items-center justify-center">
+                  <p
+                    
+                    className="relative inline-flex items-center justify-center h-12 text-white "
+                  >
+                    <img
+                      src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/44/Microsoft_logo.svg/2048px-Microsoft_logo.svg.png"
+                      alt="user name"
+                      title="user name"
+                      className="max-w-full w-16 "
+                    />
+                  </p>
+                  <div>
+                    <h3 className="text-xl font-bold text-sky-500 ">
+                      Dermatologist
+                    </h3>
+                    <h3 className="text-lg font-medium text-slate-600">
+                      Satya Skin & Hair care pvt ltd
+                    </h3>
+                    <p className="text-lg font-medium text-slate-400">
+                      {" "}
+                      Gurugram, Haryana, India (On-site)
+                    </p>
+                  </div>
+                </div>
+              </div>
+              <p className="text-3xl pt-5">
+                <BiBookmark />
+              </p>
+            </header>
+            <p className="text-md ml-20 flex items-center">
+              <span>Promoted</span>
+              <img src={cclogo} alt="logo" className="w-14" />
+              <span>Easy Apply</span>
+            </p>
+          </div>
+        </div>
+        <hr />
+      </div>
+    </div>
+  );
+};
+
+export default Main;
--- src/pages/JobsPage.js
+++ src/pages/JobsPage.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Sidebar from '../components/Jobs/Sidebar'
+import Main from '../components/Jobs/Main'
 
 const JobsPage = () => {
   return (
-    <div>
+    <div className='flex'>
     <Sidebar />
+    <Main />
     </div>
   )
 }
