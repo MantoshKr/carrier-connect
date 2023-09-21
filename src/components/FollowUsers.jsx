135b69953781f3f605ffcf02acfab0626daf16b7
mantoshdev <123961664+mantoshdev2@users.noreply.github.com>
2023-09-21 09:13:30 +0530

    Implemented user click event in FollwUser component using UserContext for data filtering in Feed
    Description:
    In this commit, I  implemented a user click event in the  `FollowUser` component just like the ConnectToUser Component. When a user clicks on a profile image, the user ID (uid) is captured and sent to the `Feed` component using the `UserContext`. This change enables data filtering in the `Feed` component based on the clicked user's ID, allowing the display of filtered data based on the selected user.And the data is shown in the home page feed section.

--- src/components/FollowUsers.jsx
+++ src/components/FollowUsers.jsx
@@ -2,28 +2,34 @@ import React, { useEffect, useState } from "react";
 import { collection, getDocs } from "firebase/firestore";
 import { db } from "../firebase";
 import { FaPlus } from "react-icons/fa";
+import { useUser } from "../context/UserContext";
 
 // Shuffle the users array using Fisher-Yates shuffle algorithm
 function shuffleArray(array) {
-    for (let i = array.length - 1; i > 0; i--) {
-      const j = Math.floor(Math.random() * (i + 1));
-      [array[i], array[j]] = [array[j], array[i]];
-    }
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
   }
+}
 
 const FollowUsers = () => {
   const [users, setUsers] = useState([]);
   const [visibleUsers, setVisibleUsers] = useState(5); // Number of users to initially display
   const usersPerPage = 5; // Number of users to load on each "View More" click
+  const { setClickedUserId } = useUser();
 
+  const handleUserClick = (userId) => {
+    setClickedUserId(userId);
+    console.log(`User with ID ${userId} clicked.`);
+  };
 
   // Shuffle the users array
-const shuffledUsers = [...users]; 
-shuffleArray(shuffledUsers);
+  const shuffledUsers = [...users];
+  shuffleArray(shuffledUsers);
 
   useEffect(() => {
     async function fetchUserData() {
-      const usersCollection = collection(db, "users"); 
+      const usersCollection = collection(db, "users");
       const querySnapshot = await getDocs(usersCollection);
 
       const usersArray = [];
@@ -54,76 +60,60 @@ shuffleArray(shuffledUsers);
 
   console.log("users", users);
 
-
   const handleViewMoreClick = () => {
     setVisibleUsers((prevVisibleUsers) => prevVisibleUsers + usersPerPage);
   };
 
-
   return (
     <div>
-       
-      <ul >
-      {shuffledUsers.slice(0, visibleUsers).map((user) => (
-       
-        <li key={user.id} className="rounded-xl border border-gray-100 shadow-md bg-white mb-2  p-4">
-        <div className="flex items-center gap-4 mb-2">
-          <img
-            alt="Developer"
-            src={user.photoURL}
-            className="h-16 w-16 rounded-full object-cover"
-          />
+      <ul>
+        {shuffledUsers.slice(0, visibleUsers).map((user) => (
+          <li
+            key={user.id}
+            className="rounded-xl border border-gray-100 shadow-md bg-white mb-2  p-4"
+          >
+            <div className="flex items-center gap-4 mb-2">
+              <img
+                alt="Developer"
+                src={user.photoURL}
+                className="h-16 w-16 rounded-full object-cover cursor-pointer"
+                onClick={() => {
+                  if (handleUserClick) handleUserClick(user.uid);
+                  console.log("inside the imag", user.uid);
+                }}
+              />
 
-          <div>
-            <h3 className="text-lg font-medium ">
-            <p>{user.name}</p>
-            
-            </h3>
+              <div>
+                <h3 className="text-lg font-medium ">
+                  <p>{user.name}</p>
+                </h3>
 
-            <div className="text-gray-500 font-medium">
-             {user.skills}
+                <div className="text-gray-500 font-medium">{user.skills}</div>
+              </div>
             </div>
-          </div>
-        </div>
-        
 
-        <button
-  className="flex items-center justify-between gap-3 rounded-full  border border-current px-5 py-2 text-gray-500 transition-colors hover:bg-gray-100 focus:outline-none focus:ring active:bg-gray-500"
-  
->
-
-<span
-    className=" bg-white  "
-  >
-    <FaPlus
-      className=" text-gray-400 text-sm  "
-      
-    />
-     
-  
-  </span>
-  <span className="font-bold text-gray-400 transition-colors ">
-    Follow
-  </span>
-
-  
-</button>
-      </li>
-    
+            <button className="flex items-center justify-between gap-3 rounded-full  border border-current px-5 py-2 text-gray-500 transition-colors hover:bg-gray-100 focus:outline-none focus:ring active:bg-gray-500">
+              <span className=" bg-white  ">
+                <FaPlus className=" text-gray-400 text-sm  " />
+              </span>
+              <span className="font-bold text-gray-400 transition-colors ">
+                Follow
+              </span>
+            </button>
+          </li>
         ))}
       </ul>
-     
+
       {visibleUsers < users.length && (
         <div className="flex justify-center">
-        <button onClick={handleViewMoreClick}
-  class="inline-block rounded border border-indigo-600 px-12 py-3 text-sm font-medium text-indigo-600 hover:bg-indigo-600 hover:text-white focus:outline-none focus:ring active:bg-indigo-500 m-10"
-  
->
-  view more
-</button>
-</div>
+          <button
+            onClick={handleViewMoreClick}
+            class="inline-block rounded border border-indigo-600 px-12 py-3 text-sm font-medium text-indigo-600 hover:bg-indigo-600 hover:text-white focus:outline-none focus:ring active:bg-indigo-500 m-10"
+          >
+            view more
+          </button>
+        </div>
       )}
-      
     </div>
   );
 };
